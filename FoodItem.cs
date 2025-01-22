728ab3e690f561a2310cb3bbf24dc121a7c36b33
Nouanounou <ja.nouanounou@gmail.com>
2025-01-22 13:43:10 -0700

    Add project files.

---
+++ FoodItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mission3Assignment
+{
+    internal class FoodItem
+    {
+
+        public Name {};
+
+        public Category {};
+
+        public Quantity {};
+        public Expiration Date { };
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Xml.Linq;
+
+Action Name = "";
+
+System.Console.WriteLine("Hello, welcome to our Local Food Bank Inventory System!");
+
+System.Console.WriteLine("Welcome to our menu. Please enter an action to take based on our menu: ");
+
+System.Console.WriteLine("Here are your options: Add Food Items, Delete Food Items, Print List of Current Food Items, Exit the Program");
+
+Name = System.Console.ReadLine();
+
+
+
